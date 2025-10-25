2ffa6fd4771c2ed7c1c735d0e198e4fbfd002a1d
Gayatri Taneja <63981052+gayatritaneja@users.noreply.github.com>
2025-10-25 08:35:47 +0530

    update

---
+++ rust-version/projects/pset-1/src/q1.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main()
+{
+    let mut n = String::new;
+
+    io::stdin()
+        .read_line(&mut n);
+        .expect("Failed to read line");
+
+    let number: i32 = match
+}
